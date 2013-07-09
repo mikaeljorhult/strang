1fee60260e5e3b6fbdfbe746ed512935d08c3c7d
Mikael Jorhult <mikael@jorhult.se>
2013-07-09 10:43:55 +0200

    Version 1

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+(function($, $document, undefined) {
+	/*--- VARIABLES ---*/
+	
+	
+	
+	
+	
+	/*--- DELEGATE ---*/
+	
+	
+	
+	
+	
+	/*--- FUNCTIONS ---*/
+	
+	
+	
+	
+	
+	/*--- DOM READY ---*/
+	$document.ready(function() {
+		
+	});
+})(jQuery, jQuery(document));
