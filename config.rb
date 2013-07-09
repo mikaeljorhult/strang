8207ae24b065351c48ebf59a6e82aec450023430
Mikael Jorhult <mikael@jorhult.se>
2013-07-09 11:30:52 +0200

    Compress CSS by default.

--- config.rb
+++ config.rb
@@ -10,13 +10,13 @@ cache_dir = "../../.sass-cache"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+#output_style = :expanded
 #output_style = :compact
-#output_style = :compressed
+output_style = :compressed
 environment = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
