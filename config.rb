f1e11677386dc995b96c27a7dc58e6a44b696e3f
Mikael Jorhult <mikael@jorhult.se>
2013-12-25 22:31:47 +0100

    Background color for HTML element.

--- config.rb
+++ config.rb
@@ -1,8 +1,6 @@
 # Make sure UTF-8 is used.
 Encoding.default_external = "utf-8"
 
-# Require any additional compass plugins here.
-
 # Set this to the root of your project when deployed:
 http_path	= "/"
 css_dir		= "assets/css"
